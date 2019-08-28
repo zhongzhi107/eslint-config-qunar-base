4527653eb7620ad4673d723cba808d73067a07cf
zhi.zhong <zhong.zhi@163.com>
2019-08-28 16:48:22 +0800

    fix: 'import/no-cycle': 'off'

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ module.exports = {
   plugins: ['qunar'],
   rules: {
     // 函数复杂度
-    complexity: 0,
-    // 'import/default': 'off',
-    // 'import/no-duplicates': 'off',
-    // 'import/named': 'off',
-    // 'import/namespace': 'off',
-    // // 检查依赖文件是否存在（文件名区分大小写）
-    // 'import/no-unresolved': ['error', { caseSensitive: true }],
+    complexity: 'off',
+    'import/no-cycle': 'off',
     'import/no-extraneous-dependencies': 'off',
     'class-methods-use-this': 'off',
     // 强制对象的最后一项不能有逗号
@@ -18,10 +13,10 @@ module.exports = {
     'no-console': 'off',
     'no-param-reassign': 'off',
     // 允许在 for 循环中使用自增/自减
-    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     // 强制 eslint-disable 后必须跟具体规则名称
-    'qunar/no-abusive-eslint-disable': 2,
+    'qunar/no-abusive-eslint-disable': 'error',
     // 强制要求数组长度做比较运算
-    'qunar/explicit-length-check': 2
+    'qunar/explicit-length-check': 'error'
   }
 };
